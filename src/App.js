f5f2508d9dad571a930b2758b974f4cc0427d37a
Merdingen <merttartac@gmail.com>
2022-05-17 20:48:04 +0300

    movie component and features added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { GlobalStyle } from './GlobalStyle';
+//Routing
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//Components
 import Home from './Mertflix/Home';
 import Header from './Mertflix/components/Header';
+import Movie from './Mertflix/Movie';
+import NotFound from './Mertflix/NotFound';
 
-function App() {
-  return (
-    <div className="App">
-      <GlobalStyle/>
-      <Header/>
-      <Home/>
-    </div>
-  );
-}
+//Styles
+import { GlobalStyle } from './GlobalStyle';
+
+const App = () => (
+  <Router>
+    <Header />
+    
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/:movieId' element={<Movie />} />
+      <Route path='/*' element={<NotFound />} />
+    </Routes>
+    
+    <GlobalStyle />
+  </Router>
+);
 
 export default App;
--- src/Mertflix/components/Header/index.js
+++ src/Mertflix/components/Header/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import MertflixLogo from '../../../images/mertflix-logo.svg';
 import TmdbLogo from '../../../images/tmdb_logo.svg';
 
@@ -8,8 +10,10 @@ import { Wrapper, Content, MertflixLogoImg, TmdbLogoImg } from './Header.styles'
 const Header = () => (
   <Wrapper>
     <Content>
-      <MertflixLogoImg src={MertflixLogo} alt='mertflix-logo'/>
-      <TmdbLogoImg src={TmdbLogo} alt='tmdb-logo'/>
+      <Link to='/'>
+        <MertflixLogoImg src={MertflixLogo} alt='mertflix-logo' />
+      </Link>
+      <TmdbLogoImg src={TmdbLogo} alt='tmdb-logo' />
     </Content>
   </Wrapper>
 );
